56032c7ddaafa9000a89c016056fe0a522b90350
Emerson J Silva <emerson.juan07@gmail.com>
2024-02-22 21:58:12 -0800

    Add screen calls from page to page
    Login and Register

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -4,7 +4,7 @@ plugins {
 
 android {
     namespace = "com.example.thebookhuntingeagle"
-    compileSdk = 33
+    compileSdk = 34
 
     defaultConfig {
         applicationId = "com.example.thebookhuntingeagle"

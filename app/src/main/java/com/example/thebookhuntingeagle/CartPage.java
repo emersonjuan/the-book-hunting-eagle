67bbffc60ac6cbb9aec0ff5a0751cec7229e852b
PatriciaMatsumura <152531079+PatriciaMatsumura@users.noreply.github.com>
2024-02-23 18:44:29 -0800

    Added more pages to the app
    Added 7 more pages to the app and changed some layouts.

---
+++ app/src/main/java/com/example/thebookhuntingeagle/CartPage.java
@@ -0,0 +1,14 @@
+package com.example.thebookhuntingeagle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class CartPage extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_cart_page);
+    }
+}
---
+++ app/src/main/java/com/example/thebookhuntingeagle/SellSharePage.java
@@ -0,0 +1,14 @@
+package com.example.thebookhuntingeagle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class SellSharePage extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_sell_share_page);
+    }
+}

c9ebe4a3e76c654f5b8cdeb7c492b4385a5cdbce
Emerson J Silva <emerson.juan07@gmail.com>
2024-03-24 20:28:50 -0700

    add splash logout_page and main_activity

--- app/src/main/java/com/example/thebookhuntingeagle/LogoutPage.java
+++ app/src/main/java/com/example/thebookhuntingeagle/LogoutPage.java
@@ -2,7 +2,11 @@ package com.example.thebookhuntingeagle;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+
+import java.util.Timer;
+import java.util.TimerTask;
 
 public class LogoutPage extends AppCompatActivity {
 
@@ -10,5 +14,14 @@ public class LogoutPage extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_logout_page);
+        TimerTask task = new TimerTask( ) {
+            @Override
+            public void run(){
+                finish();
+                startActivity(new Intent(LogoutPage.this, MainActivity.class));
+            }
+        };
+        Timer opening = new Timer();
+        opening.schedule(task,3000);
     }
 }
